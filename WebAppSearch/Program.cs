0343b9c0add83f4e83d7010a80e4a8070a8985b0
Fawad Nassery <eaa23fana@students.eaaa.dk>
2026-03-03 15:08:59 +0100

    Loadbalance toggle in UI

--- WebAppSearch/Program.cs
+++ WebAppSearch/Program.cs
@@ -1,5 +1,4 @@
 using WebAppSearch.Components;
-
 namespace WebAppSearch
 {
     public class Program
@@ -7,34 +6,19 @@ namespace WebAppSearch
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
-
-            builder.Services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri("http://localhost:5144")
-            });
-
+            builder.Services.AddScoped(sp => new HttpClient());
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            //app.UseHttpsRedirection();
-
             app.UseStaticFiles();
             app.UseAntiforgery();
-
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
-
             app.Run();
         }
     }
