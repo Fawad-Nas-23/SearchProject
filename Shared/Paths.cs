7635e3f1322f7ee2cdf21b59668721f8337a9fac
Hudayfa <hhw3103@hotmail.com>
2026-04-27 12:26:02 +0200

    Det hele er sat op til at køre

--- Shared/Paths.cs
+++ Shared/Paths.cs
@@ -1,8 +1,11 @@
 ﻿namespace Shared;
-    public class Paths
-    {
-        public static readonly string SQLITE_DATABASE_1 = @"C:\6.Semester\ArkitekturPrincipper\SearchData\SearchDB-1.db";
-        public static readonly string SQLITE_DATABASE_2 = @"C:\6.Semester\ArkitekturPrincipper\SearchData\SearchDB-2.db";
-        public static readonly string POSTGRES_DATABASE = "Host=127.0.0.1;Port=5432;Username=postgres;Password=1234;Database=SearchDB";
-    }
+
+public class Paths
+{
+    public static readonly string SQLITE_DATABASE_1 = @"C:\6.Semester\ArkitekturPrincipper\SearchData\SearchDB-1.db";
+    public static readonly string SQLITE_DATABASE_2 = @"C:\6.Semester\ArkitekturPrincipper\SearchData\SearchDB-2.db";
+    //public static readonly string POSTGRES_DATABASE = "Host=127.0.0.1;Port=5432;Username=postgres;Password=1234;Database=SearchDB";
+    public static readonly string POSTGRES_DATABASE = "Host=127.0.0.1;Port=5433;Username=postgres;Password=1234;Database=SearchDB";
+
+}
 
--- indexer/Config.cs
+++ indexer/Config.cs
@@ -4,6 +4,7 @@ public class Config
 {
     // the folder to be indexed - all .txt files in that folder (and subfolders)
     // will be indexed
-    public static string FOLDER = @"C:\6.Semester\ArkitekturPrincipper\seData\seData copy\large\arnold-j";
-
+    //public static string FOLDER = @"C:\6.Semester\ArkitekturPrincipper\seData\seData copy\large\arnold-j";
+    public static string FOLDER = @"C:\Users\hhw31\OneDrive\Skrivebord\ITSem6\ArkitekturPrincipper\seData\large\arnold-j";
 }
+
